dd834d9ee46f6fafc332cb38a54868a510599d11
Joseph <joe@josephmccarron.com>
2014-07-24 10:22:03 -0700

    cleanup

--- app.js
+++ app.js
@@ -21,12 +21,16 @@
     onAppCreated: function() {
       var Segment = require('segment/segment.js');
       this.segment = new Segment(this);
+      // this.segment.identifyAndGroup() generates a unique ID for the current user (subdomain+zd_user_id), and associates the user with a group identified by subdomain
+      // it also records some associated metadata defined in segment.js. 
+      // Users: name, email, id, subdomain, locale
+      // Groups: subdomain (as ID), Zendesk Plan Name (this groups your users by Zendesk subdomain)
       this.segment.identifyAndGroup();
     },
     trackClick: function(e) {
-      e.preventDefault();
-      var elementId = e.currentTarget.id;
-      this.segment.track('Clicked tracked element', {'currentTarget':elementId, 'key2':'value2'});
+      e.preventDefault(); // optional: this prevents links & buttons from performing their default actions (don't use if you actually want a link to work normally)
+      var elementId = e.currentTarget.id; // optional: this captures the ID of the clicked element to pass it into the event (next line).
+      this.segment.track('Clicked tracked element', {'currentTarget':elementId, 'key2':'value2'}); // this.segment.track() takes two arguments: 1. a string for the name of the event 2. an object of "properties" that can store anything
     }
   };
 
